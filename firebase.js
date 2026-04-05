70bbe043ddd0da8a9571cca5d681ebfae2f99ead
AdivaldoSan <professoradivaldo@gmail.com>
2026-04-05 11:11:15 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAMJqS5RrwnCAMIONNraFWIRRA2k1YsPg4",
+  authDomain: "ment-aula.firebaseapp.com",
+  databaseURL: "https://ment-aula-default-rtdb.firebaseio.com",
+  projectId: "ment-aula",
+  storageBucket: "ment-aula.firebasestorage.app",
+  messagingSenderId: "959984637634",
+  appId: "1:959984637634:web:5ada1eb50ddd63d65f4d5b"
+};
 
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
